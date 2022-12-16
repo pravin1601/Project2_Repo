ee3c8726d823ef18f1704725ab9d06b484c4caae
pravin1601 <82778292+pravin1601@users.noreply.github.com>
2022-12-16 20:54:16 +0530

    updated config

--- website/config.php
+++ website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website made by Pravin Pradhan',
+        'name' => 'Simple PHP Website made by Pravin B Pradhan',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
